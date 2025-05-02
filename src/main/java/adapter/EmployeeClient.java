a420544a59aad398f3bdb7bbba9c9c47af08b90e
jamesmwangi55 <jamesmwangi55@gmail.com>
2025-05-02 06:04:37 +0300

    Add adapter for ldap

--- src/main/java/adapter/EmployeeClient.java
+++ src/main/java/adapter/EmployeeClient.java
@@ -15,9 +15,11 @@ public class EmployeeClient {
 		
 		//Will not work! This is where the adapter comes into play!
 		
-		//Employee employeeFromLdap = new EmployeeLdap("chewie", "Solo", "Han", "han@solo.com");
+//		Employee employeeFromLdap = new EmployeeLdap("chewie", "Solo", "Han", "han@solo.com");
 
 		EmployeeLdap employeeFromLdap = new EmployeeLdap("chewie", "Solo", "Han", "han@solo.com");
+
+		employees.add(new EmployeeAdapterLdap(employeeFromLdap));
 
 		return employees;
 		
